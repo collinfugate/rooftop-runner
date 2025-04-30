200ddc1ed34cad3f7e0e779882328a157e3b2250
Collin Fugate <137461088+collinfugate@users.noreply.github.com>
2025-04-29 23:45:22 -0400

    add background image and jump/run animations

--- src/game.js
+++ src/game.js
@@ -29,9 +29,32 @@ function preload() {
 		frameHeight: 32
 	});
 
-	this.load.image("ground", "https://labs.phaser.io/assets/sprites/platform.png");
+	this.load.image("platform", "https://labs.phaser.io/assets/sprites/platform.png");
+
+	this.load.image("background", "/assets/backgrounds/city 1/10.png");
+
 }
 function create() {
+
+	this.add.image(0, 0, "background").setOrigin(0, 0).setScrollFactor(0).setDisplaySize(800, 600);
+
+	// animations
+	this.anims.create({
+		key: "run",
+		frames: [
+			{ key: "ninja", frame: 1 },
+			{ key: "ninja", frame: 8 }
+		],
+		frameRate: 8,
+		repeat: -1
+	});
+
+	this.anims.create({
+		key: "jump",
+		frames: [{ key: "ninja", frame: 9 }],
+		frameRate: 1,
+		repeat: -1
+	});
 
 	// prevents Phaser from smoothing the pixel art when scaled
 	this.textures.get("ninja").setFilter(Phaser.Textures.FilterMode.NEAREST);
@@ -40,6 +63,7 @@ function create() {
 	this.cameras.main.startFollow(player);
 	player.setVelocityX(RUN_SPEED);
 	player.setScale(3);
+	player.anims.play("run", true);
 
 	// set custom hitbox size to match ninja’s visible body
 	player.body.setSize(12, 15, true);
@@ -48,12 +72,14 @@ function create() {
 	const platforms = this.physics.add.staticGroup();
 
 	// platforms
-	const ground1 = platforms.create(400, 500, "ground");
-	ground1.setScale(2).refreshBody();
-	const ground2 = platforms.create(1600, 500, "ground");
-	ground2.setScale(2).refreshBody();
-	const ground3 = platforms.create(2800, 500, "ground");
-	ground3.setScale(2).refreshBody();
+	const platform1 = platforms.create(400, 500, "platform");
+	platform1.setScale(2).refreshBody();
+
+	const platform2 = platforms.create(1600, 500, "platform");
+	platform2.setScale(2).refreshBody();
+
+	const platform3 = platforms.create(2800, 500, "platform");
+	platform3.setScale(2).refreshBody();
 
 	this.physics.add.collider(player, platforms);
 	this.input.keyboard.on("keydown-SPACE", () => {
@@ -61,6 +87,13 @@ function create() {
 	});
 }
 
-function update() { }
+// switch between run and jump animations based on grounded state
+function update() {
+	if (!player.body.blocked.down) {
+		player.anims.play("jump", true);
+	} else {
+		player.anims.play("run", true);
+	}
+}
 
 new Phaser.Game(config);
